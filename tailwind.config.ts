bbbd16365709e5ebb541c2e77582955179c3b015
ZenShibata <zen@frutbits.org>
2023-09-17 19:00:26 +0700

    chore(tailwind): extend colors
    Co-authored-by: Achmad Tirto Sudiro <118708640+Achmadts@users.noreply.github.com>
    Co-authored-by: Dean Akmal <118705266+DeanAkmal@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {},
-  plugins: [],
-}
-export default config
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            colors: {
+                night: "#0F1707",
+                honeydew: "#E8F4DC",
+                pistachio: "#99CD65",
+                teagreen: "#CFE7B6",
+                asparagus: "#699E33",
+                success: "#8ACC47",
+                info: "#C3E9D7",
+                warning: "#DCE79A",
+                danger: "#E78476",
+            },
+        },
+    },
+    plugins: [],
+};
+export default config;
