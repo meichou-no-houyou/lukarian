f9f95c3ca680d079211aa1d17e9a4113a2a34643
ZenShibata <zen@clytage.org>
2023-09-21 03:24:46 +0700

    feat(button): add anchor options

--- src/app/lib/components/button.tsx
+++ src/app/lib/components/button.tsx
@@ -1,4 +1,5 @@
 import { mc } from "@/app/lib/utils/MergeClass";
+import Link from "next/link";
 import { MouseEventHandler } from "react";
 
 const colors = {
@@ -6,14 +7,31 @@ const colors = {
     success: "bg-success text-white focus:ring-success",
     danger: "bg-danger text-white focus:ring-danger",
     info: "bg-info text-night focus:ring-info",
+    transparent: "bg-transparent focus:ring-transparent",
 };
 
-export default function Button({ children, type, className, onClick }: { children: React.ReactNode; type: keyof typeof colors; className?: string; onClick?: MouseEventHandler<HTMLButtonElement> }) {
-    return (
-        <button
-            onClick={onClick}
-            className={mc(`${colors[type]} rounded-[10px] font-bold text-md`, `hover:opacity-80 focus:ring-4 focus:ring-opacity-25 focus:outline-none`, "py-2 px-3 md:py-2.5 md:px-5", className)}
-        >
+export default function Button({
+    children,
+    type,
+    className,
+    onClick,
+    asAnchor,
+    href,
+}: {
+    children: React.ReactNode;
+    type: keyof typeof colors;
+    className?: string;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+    asAnchor?: boolean;
+    href?: string;
+}) {
+    const classes = mc(`${colors[type]} rounded-[10px] font-bold text-md`, `hover:opacity-80 focus:ring-4 focus:ring-opacity-25 focus:outline-none`, "py-2 px-3 md:py-2.5 md:px-5", className);
+    return asAnchor ? (
+        <Link href={href ?? "/"} className={classes} role="button">
+            {children}
+        </Link>
+    ) : (
+        <button onClick={onClick} className={classes}>
             {children}
         </button>
     );
