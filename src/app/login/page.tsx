ad71f72e07b8bbb1b2c1369b240fe7676e2d28f4
Achmad <achmadtirtosudirosudiro@gmail.com>
2023-09-22 08:52:36 +0700

    fix(login): adjust responsiveness

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,16 +12,17 @@ export default function Login() {
             <Navbar />
             <div className="h-[5rem]"></div>
 
-            <div className="w-full grid grid-rows-4 grid-cols-1 md:grid-rows-1 md:grid-cols-3">
-                <div className="bg-honeydew w-full flex flex-col justify-evenly items-start px-12">
-                    <div className="max-w-lg mx-auto flex flex-col mb-auto">
-                        <h1 className="mt-10 font-bold text-3xl text-center ">Selamat Datang Kembali</h1>
-                        <p className="text-base text-justify">Kamu bisa melanjutkan perjalananmu mempelajari cara menangani luka dengan login terlebih dahulu.</p>
-                        <p className="py-8">Tidak punya akun? <Link href={"/"} className="text-blue-600 font-bold">Daftar</Link> sekarang</p>
+            <div className="flex flex-col md:flex-row">
+                <div className="bg-honeydew w-full md:w-1/3 flex flex-col items-start">
+                    <div className="px-12 mx-auto flex flex-col gap-y-2">
+                        <div className="h-[2.5rem]"></div>
+                        <h1 className="font-bold text-3xl">Selamat Datang</h1>
+                        <p>Bergabunglah dengan LukaRian! Daftarkan diri Anda sekarang untuk mendapatkan akses eksklusif untuk informasi terbaru.</p>
+                        <p className="py-8">Sudah punya akun? <span className="text-blue-700 font-bold"><Link href="/login">Masuk</Link></span> sekarang!</p>
                         <UndrawDoctorSplash width={348} height={286} className="hidden md:block"></UndrawDoctorSplash>
                     </div>
                 </div>
-                <div className="flex items-center col-span-2 justify-center">
+                <div className="flex items-center justify-center flex-1">
                     <div className="h-screen"></div>
 
                     <form>
@@ -29,26 +30,26 @@ export default function Login() {
                             <h1 className="font-bold text-center text-4xl">Masuk</h1>
                             <br />
                             <div className="flex gap-x-14 ">
-                            <Button type="success" className="rounded-full p-2 md:p-2" >
-                                <IconBrandGoogle /> 
-                            </Button>
+                                <Button type="success" className="rounded-full p-2 md:p-2" >
+                                    <IconBrandGoogle />
+                                </Button>
 
-                            <Button type="success" className="rounded-full p-2 md:p-2" >
-                            <IconBrandDiscord/> 
-                            </Button>
-                            
-                            <Button type="success" className="rounded-full p-2 md:p-2" >
-                            <IconBrandFacebook/>
-                            </Button>
-                    
+                                <Button type="success" className="rounded-full p-2 md:p-2" >
+                                    <IconBrandDiscord />
+                                </Button>
+
+                                <Button type="success" className="rounded-full p-2 md:p-2" >
+                                    <IconBrandFacebook />
+                                </Button>
+
                             </div>
                             <br />
                             <input type="text" className="rounded-md mb-2 w-80 h-9 border-slate-500 border px-2" placeholder="Username" />
-                            <input type="password" className="rounded-md mb-2 w-80 h-9 border-slate-500 border px-2" placeholder="Password"   />
+                            <input type="password" className="rounded-md mb-2 w-80 h-9 border-slate-500 border px-2" placeholder="Password" />
                             <br />
                             <p className="font-bold text-sm text-center">Lupa Password? <Link href={""} className="text-blue-600">Reset</Link> disini</p>
                             <br />
-                            <Button type = "success">Masuk</Button>
+                            <Button type="success">Masuk</Button>
                         </div>
                     </form>
                 </div>
